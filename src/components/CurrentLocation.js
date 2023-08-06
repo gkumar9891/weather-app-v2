87a0e72025b29d2b6523d01b92fb19d80911000f
Gauav Kumar <54814034+gkumar9891@users.noreply.github.com>
2023-08-06 14:12:22 +0000

    added axios

--- src/components/CurrentLocation.js
+++ src/components/CurrentLocation.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
 const CurrentLocation = () => {
-    return (
+
+const [currentTime, setCurrentTime] = useState(new Date());
+const [weather, setWeather] = useState({});
+
+useEffect(() => {
+  setTimeout(setCurrentTime(new Date()), 1000);
+}, [currentTime])
+
+function geLatLong() {
+  let lat;
+  let long;
+
+  return [lat, long]
+}
+
+return (
         <div className="weather-content">
         <div className="place text-end">
           <p className="city">Vikaspuri</p>
@@ -7,8 +23,8 @@ const CurrentLocation = () => {
         </div>
         <div className="footer px-3">
           <div className="date-content">
-            <p className="text-large">18:31:00</p>
-            <p className="text-small">Wednesday, 15 February 2023</p>
+            <p className="text-large">{currentTime.toLocaleTimeString()}</p>
+            <p className="text-small">{currentTime.toDateString()}</p>
           </div>
           <p className="large1 text-end flex-grow-1">
             23<sup>o</sup>c
