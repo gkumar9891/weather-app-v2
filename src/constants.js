48716bb1024d08e79ca854c772b47615dc039ad3
gkumar9891 <gauravkumargk9892@gmail.com>
2024-10-11 00:04:51 +0530

    added env file

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,3 @@
-//rename constants.example to constants.js and replace the values
-
-export const APP_ID="API_ID_HERE";
+export const APP_ID=process.env.REACT_APP_WEATHER_KEY;
 export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
 export const WEATHER_ICON_URL="https://openweathermap.org/img/wn";
